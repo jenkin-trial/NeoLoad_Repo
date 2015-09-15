a3c9f93f0a49746390c018f31e681f0fb4a669db
pallavi <pallavi.snehal@accenture.com>
2015-09-15 14:04:00 +0530

    NEW APP

--- src/com/example/neoload_app/Neoload.java
+++ src/com/example/neoload_app/Neoload.java
@@ -42,7 +42,7 @@ Button b;
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		 goToUrl ( "http://personal.aib.ie/");
+		 goToUrl ( "www.vodacom.com/");
 	}
 	 private void goToUrl (String url) {
 	        Uri uriUrl = Uri.parse(url);
