c83e2448307e1e7032fa352c89da11619c20d641
pallavi <pallavi.snehal@accenture.com>
2015-10-14 17:12:45 +0530

    NBC url

--- src/com/example/neoload_app/Neoload.java
+++ src/com/example/neoload_app/Neoload.java
@@ -42,7 +42,7 @@ Button b;
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		 goToUrl ( "http://www.walmart.com/");
+		 goToUrl ( "https://www.nbc.ca/en/personal.html");
 	}
 	 private void goToUrl (String url) {
 	        Uri uriUrl = Uri.parse(url);
